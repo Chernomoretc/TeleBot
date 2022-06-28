1bf4d3cf26e43562b4ff984c6783812fa73ada32
Stas Chernomoretc <stas.chernomoretc@gmail.com>
2022-06-28 10:05:23 +0800

    Hello world

---
+++ src/test/java/com/work/TeleBot/TeleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.TeleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
